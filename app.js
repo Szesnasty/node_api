828c5089f28d9cb639d29da550a139afbe1c64eb
Pan Szesnasty <lukasz.jakubowski1993@gmail.com>
2020-09-12 11:13:16 +0200

    connect to database

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
 // ROUTES
-app.get('/', (req, res)=>{
-    res.send('Its work!')
-})
+app.get("/", (req, res) => {
+  res.send("Its work!");
+});
+
+// CONNECT
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("connect!!")
+);
 
 app.listen(3000);
