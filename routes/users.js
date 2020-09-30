28499de8c883e822cd2551601bc2fb8b0368b271
Pan Szesnasty <lukasz.jakubowski1993@gmail.com>
2020-09-30 16:40:34 +0200

    hash password

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const express = require("express");
 const Users = require("./../models/Users");
 const router = express.Router();
 const { regiserValidation } = require("../validations/users/users-validation");
-const Joi = require("@hapi/joi");
+const bcrypt = require("bcryptjs");
+
 // Get all users
 router.get("/", async (req, res) => {
   try {
@@ -18,12 +19,20 @@ router.post("/", async (req, res) => {
   const { error } = regiserValidation(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
+
+  // Checking if user already exist in database
+  const emailExist = await Users.findOne({ email: req.body.email });
+
+  const salt = await bcrypt.genSalt(20);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
   const users = new Users({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPassword,
   });
-
+  if (emailExist)
+    return res.status(400).send("User with that email already exist");
   try {
     const savedUser = await users.save();
     res.send(savedUser);
@@ -46,7 +55,7 @@ router.get("/:userId", async (req, res) => {
 router.delete("/:userId", async (req, res) => {
   try {
     const singleUser = await Users.deleteOne({ _id: req.params.userId });
-    res.json(users);
+    res.json(singleUser);
   } catch (err) {
     res.json({ message: err });
   }
