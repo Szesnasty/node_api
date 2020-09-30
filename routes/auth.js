a346e126a9762ea4738f2a67ef5b2cc88851f134
Pan Szesnasty <lukasz.jakubowski1993@gmail.com>
2020-09-30 17:00:56 +0200

    import Users model to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { loginValidation } = require("../validations/users/users-validation");
 const bcrypt = require("bcryptjs");
+const Users = require("./../models/Users");
 
-router.post("/login", async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = loginValidation(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
